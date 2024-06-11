d17bb275de0638dcc3298445f24e8c24c78cfb28
Josue Borden <josue.borden6@gmail.com>
2024-06-10 23:48:32 -0600

    Creacion de interfaz y boton Salir

---
+++ src/josueborden_proyectop1/JosueBorden_ProyectoP1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package josueborden_proyectop1;
+
+/**
+ *
+ * @author PC
+ */
+public class JosueBorden_ProyectoP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana pantalla = new Ventana();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
